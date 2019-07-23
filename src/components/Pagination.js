47b6994ac35ab43b1187102579d694de73bb3810
Przemysław Hennig <przemyslaw.hennig@gmail.com>
2019-07-24 00:32:21 +0200

    Pagination implemented

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,34 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Pagination = ({ totalItems, pageLimit = 20, handlePageNumber }) => {
+const Pagination = ({ totalItems, handleOffset, offset, itemsLimit = 20 }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+
   const totalPages = () => {
-    return Math.ceil(totalItems / pageLimit);
+    return Math.ceil(totalItems / itemsLimit);
   };
 
-  const pageNumbers = [];
+  const pages = [];
   for (let i = 1; i <= totalPages(); i++) {
-    pageNumbers.push(i);
+    pages.push(i);
   }
 
+  const paginationPages = () => {
+    const leftPages = [1, '«'];
+    const middlePages = pages.slice(Number(currentPage) - 2, Number(currentPage) + 1);
+    const rightPages = ['»', totalPages()];
+
+    const pagesWithLeft = [...leftPages, ...pages.slice(totalPages() - 5)];
+    const pagesWithLeftRight = [...leftPages, ...middlePages, ...rightPages];
+    const pagesWithRight = [...pages.slice(0, 5), ...rightPages];
+
+    const showAll = totalPages() <= 7;
+    const showRight = currentPage <= 3;
+    const showLeft = currentPage >= totalPages() - 2;
+
+    if (showAll) return pages;
+    else if (showRight) return pagesWithRight;
+    else if (showLeft) return pagesWithLeft;
+    else return pagesWithLeftRight;
+  };
+
+  const handleClick = (e) => {
+    handleOffset(e.target.id * itemsLimit - itemsLimit);
+    setCurrentPage(e.target.id);
+  };
+
+  const handleClickLeft = () => {
+    handleOffset(offset - itemsLimit * 3);
+    setCurrentPage(Number(currentPage) - 3);
+  };
+
+  const handleClickRight = () => {
+    handleOffset(offset + itemsLimit * 3);
+    setCurrentPage(Number(currentPage) + 3);
+  };
+
+  if (!totalItems || totalItems <= itemsLimit) return null;
+
   return (
-    <ul>
-      {pageNumbers.map((pageNumber) => (
-        <li
-          key={pageNumber}
-          id={pageNumber}
-          onClick={(event) =>
-            handlePageNumber(event.target.id * pageLimit - pageLimit)
-          }
-          style={{
-            display: 'inline-block',
-            marginRight: '10px',
-            cursor: 'pointer',
-          }}
-        >
-          {pageNumber}
-        </li>
-      ))}
-    </ul>
+    <nav>
+      <ul>
+        {paginationPages().map((paginationPage, index) => {
+          if (paginationPage === '«')
+            return (
+              <li key={index}>
+                <a href="#" onClick={handleClickLeft}>
+                  {paginationPage}
+                </a>
+              </li>
+            );
+
+          if (paginationPage === '»')
+            return (
+              <li key={index}>
+                <a href="#" onClick={handleClickRight}>
+                  {paginationPage}
+                </a>
+              </li>
+            );
+
+          return (
+            <li key={index}>
+              <a href="#" id={paginationPage} onClick={handleClick}>
+                {paginationPage}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <p>{`Current page: ${currentPage}`}</p>
+      {offset}
+    </nav>
   );
 };
 
--- src/containers/AgenciesFetch.js
+++ src/containers/AgenciesFetch.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useState, useEffect } from 'react';
 
 import useDataFetch from 'hooks/useDataFetch';
-import api from 'services/api';
+import API from 'services/api';
 import Error from 'components/Error';
 import Loading from 'components/Loading';
 import Pagination from 'components/Pagination';
@@ -9,35 +9,38 @@ import Pagination from 'components/Pagination';
 const AgenciesFetch = () => {
   const [{ data, isLoading, isError }, doFetch] = useDataFetch(
     { items: [], totalItems: 0 },
-    api.agencies
+    API.agencies
   );
   const [offset, setOffset] = useState(0);
-  const [pageLimit, setPageLimit] = useState(20);
+  const itemsLimit = 20;
 
   useEffect(() => {
-    doFetch(api.agencies + `&limit=${pageLimit}&offset=${offset}`);
+    doFetch(API.agencies + `&offset=${offset}&limit=${itemsLimit}`);
   }, [offset]);
 
   return (
     <Fragment>
+      {isError && <Error />}
+
       <Pagination
         totalItems={data.totalItems}
-        pageLimit={pageLimit}
-        handlePageNumber={setOffset}
+        handleOffset={setOffset}
+        offset={offset}
+        itemsLimit={itemsLimit}
       />
-
-      {isError && <Error />}
 
       {isLoading ? (
         <Loading />
       ) : (
-        <ul>
-          {data.items.map((item) => (
-            <li key={item.id}>
-              <a href="/">{item.name}</a>
-            </li>
-          ))}
-        </ul>
+        <Fragment>
+          <ul>
+            {data.items.map((item) => (
+              <li key={item.id}>
+                <a href="/">{item.name}</a>
+              </li>
+            ))}
+          </ul>
+        </Fragment>
       )}
     </Fragment>
   );
