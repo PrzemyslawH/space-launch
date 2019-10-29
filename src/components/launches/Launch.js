d65caac7ee6e071bf03c33578f17c9173cfbd950
Przemysław Hennig <przemyslaw.hennig@gmail.com>
2019-10-29 21:10:44 +0100

    Counter bug fix

--- src/components/launches/Launch.js
+++ src/components/launches/Launch.js
@@ -19,28 +19,37 @@ const Launch = () => {
 
   const { name, pad, net, status, rocket, mission } = item;
 
+  const showCounter = () => {
+    let todayMilis = Date.parse(new Date());
+    let launchMilis = Date.parse(net);
+
+    if (launchMilis > todayMilis) return true;
+  };
+
   return (
     <>
       <SectionGrid>
         <div>
           <BorderLeft>
             <H2>{name}</H2>
-            <ItemSubtitle props={pad.location.name}/>
+            <ItemSubtitle props={pad.location.name} />
           </BorderLeft>
         </div>
         <SectionDetails>
-          <H4 primary>Time to launch</H4>
-          <Counter>
-            <CounterContainer upcomingLaunchDate={net} />
-          </Counter>
+          {showCounter() && (
+            <>
+              <H4 primary>Time to launch</H4>
+              <Counter>
+                <CounterContainer upcomingLaunchDate={net} />
+              </Counter>
+            </>
+          )}
           <H4>Launch Date</H4>
           <Info>
-            <LaunchDate upcomingLaunchDate={net}/>
+            <LaunchDate upcomingLaunchDate={net} />
           </Info>
           <H4>Status</H4>
-          <Info>
-            {status.name}
-          </Info>
+          <Info>{status.name}</Info>
         </SectionDetails>
       </SectionGrid>
 
@@ -52,13 +61,9 @@ const Launch = () => {
         </div>
         <SectionDetails>
           <H4>Name</H4>
-          <Info>
-            {rocket.configuration.name}
-          </Info>
+          <Info>{rocket.configuration.name}</Info>
           <H4>Launch service provider</H4>
-          <Info>
-            {rocket.configuration.launch_service_provider.name}
-          </Info>
+          <Info>{rocket.configuration.launch_service_provider.name}</Info>
         </SectionDetails>
       </SectionGrid>
 
@@ -70,17 +75,11 @@ const Launch = () => {
         </div>
         <SectionDetails>
           <H4>Name</H4>
-          <Info>
-            {mission.name}
-          </Info>
+          <Info>{mission.name}</Info>
           <H4>Type</H4>
-          <Info>
-            {mission.type}
-          </Info>
+          <Info>{mission.type}</Info>
           <H4>Description</H4>
-          <Description>
-            {mission.description}
-          </Description>
+          <Description>{mission.description}</Description>
         </SectionDetails>
       </SectionGrid>
 
@@ -92,13 +91,9 @@ const Launch = () => {
         </div>
         <SectionDetails>
           <H4>Name</H4>
-          <Info>
-            {pad.name}
-          </Info>
+          <Info>{pad.name}</Info>
           <H4>Location</H4>
-          <Info>
-            {pad.location.name}
-          </Info>
+          <Info>{pad.location.name}</Info>
         </SectionDetails>
       </SectionGrid>
     </>
